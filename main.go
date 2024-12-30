d891ffe436eba72f8ae729d0fd333d02844eb896
yadier <yadierg7@gmail.com>
2024-12-30 19:53:13 -0400

    updated

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+	r := gin.Default()
+	r.GET("/ping", pong)
+	r.Run(":8080") // listen and serve on localhost:8080
+}
+
+func pong(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
 }
