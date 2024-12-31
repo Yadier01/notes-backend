81bbee838668ed1d8a2b41e93759f8fcb3364853
yadier <yadierg7@gmail.com>
2024-12-30 20:21:11 -0400

    updated

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"net/http"
 
+	"github.com/Yadier01/notes-backend/db"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	r := gin.Default()
+
+	db.InitDB()
 	r.GET("/ping", pong)
 	r.Run(":8080") // listen and serve on localhost:8080
 
-	// Prueba
-	var xd = "s"
 }
 
 func pong(c *gin.Context) {
